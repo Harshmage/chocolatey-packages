24bb48fd1990a7e7e6254e1e47d476c1bf62abac
Bryan Adamson <admin@harshmage.com>
2015-03-04 14:35:07 -0700

    Updated UniServer to include shortcuts on the Start Menu and a new shortcut for the plugins area on Sourceforge.

--- uniformserver/tools/chocolateyInstall.ps1
+++ uniformserver/tools/chocolateyInstall.ps1
@@ -5,7 +5,27 @@ $silentArgs = '-o"C:\" -y'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
 
+# Make the Start Menu folder
+md -Force 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Uniform Server'
+
+# Create the Desktop shortcuts
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$HOME\Desktop\UniController.lnk")
 $Shortcut.TargetPath = "C:\UniServerZ\UniController.exe"
+$Shortcut.Save()
+
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$HOME\Desktop\Uniform Server Modules.url")
+$Shortcut.TargetPath = "http://sourceforge.net/projects/miniserver/files/Uniform%20Server%20ZeroXI/ZeroXImodules/"
+$Shortcut.Save()
+
+# Create the Start Menu shortcuts
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\Uniform Server\UniController.lnk")
+$Shortcut.TargetPath = "C:\UniServerZ\UniController.exe"
+$Shortcut.Save()
+
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\Uniform Server\Uniform Server Modules.url")
+$Shortcut.TargetPath = "http://sourceforge.net/projects/miniserver/files/Uniform%20Server%20ZeroXI/ZeroXImodules/"
 $Shortcut.Save()
